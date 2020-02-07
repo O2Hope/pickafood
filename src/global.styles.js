36ae462cd23343eaa3092e5bb111aea7b7327999
O2hope <agusvaldes2@live.com>
2020-02-06 22:14:06 -0600

    Initial commit - application backbone

---
+++ src/global.styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+	body {
+        font-family: 'Roboto';
+        margin: 0px;
+
+	* {
+		box-sizing: border-box;
+	}
+`;
+
+export default GlobalStyle;
